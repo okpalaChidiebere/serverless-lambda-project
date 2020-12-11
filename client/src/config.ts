7da5deb61ba34d421b7f30dd1f6d2817eb818a9c
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-12-11 17:21:17 -0600

    Implement Request Validation
    Incoming HTTP requests are using request validation in API Gateway by providing request schemas in function definitions.
    This will make us save on lambda execution calls because the function will not be executed when the user sends invalid object

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = ''
+const apiId = 'qicjorg542'
 export const apiEndpoint = `https://${apiId}.execute-api.ca-central-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '',            // Auth0 domain
-  clientId: '',          // Auth0 client id
+  domain: 'dev-1k78x7pq.us.auth0.com',            // Auth0 domain
+  clientId: 'rxtkcEtAxSwoc1E3XdYj7ZiiJfhFzOBk',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
